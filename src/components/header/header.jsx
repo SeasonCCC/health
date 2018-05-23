5088adc4bfb69403e6012b29be420929c239a152
seasonCCC <378434086@qq.com>
2018-05-23 22:57:39 +0800

    new header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './header.scss'
+
+class Header extends React.Component {
+  render () {
+    <div className="header">Header</div>
+  }
+}
+
+export default Header
