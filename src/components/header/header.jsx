d624d38949987b27967f402771e60049f83df0f9
seasonCCC <378434086@qq.com>
2018-05-27 22:46:07 +0800

    new header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './header.scss'
+import back from '@/assets/back.png'
 
 class Header extends React.Component {
   render () {
     return (
-      <div className="header">Header</div>
+      <div className="header f">
+        <div className="back">
+          <img src={back} />
+        </div>
+        <div className="title f ac jc">
+          HEADER
+        </div>
+        <div className="tool f ac jc">
+          save
+        </div>
+      </div>
     );
   }
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import Header from '@/components/header/header'
+import './home.scss'
 
 class Home extends React.Component {
   render () {
     return (
       <div className="App">
-        <p>123</p>
+        <Header></Header>
       </div>
     );
   }
