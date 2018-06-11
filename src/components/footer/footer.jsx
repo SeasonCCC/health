1fbb5f89c030c071c630cfa4d523e4282fff8f73
xianbx <378434086@qq.com>
2018-06-11 14:32:24 +0800

    new footer and select components

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,29 +1,45 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
-import Paper from '@material-ui/core/Paper'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
 import Today from '@material-ui/icons/Today'
 import History from '@material-ui/icons/History'
 import ShowChart from '@material-ui/icons/ShowChart'
 import './footer.scss'
 
-export default class IconLabelTabs extends React.Component {
-  state = {
-    value: 'today'
-  }
-
-  handleChange = (event, value) => {
-    this.setState({value});
-  }
-
-  render() {
-    return (<Paper className="footer">
-      <Tabs value={this.state.value} onChange={this.handleChange} fullWidth indicatorColor="secondary" textColor="secondary">
-        <Tab value="today" icon={<Today />} label="TODAY" component={Link} to="/" />
-        <Tab value="history" icon={<History />} label="HISTORY" component={Link} to="/history" />
-        <Tab value="analysis" icon={<ShowChart />} label="ANALYSIS" component={Link} to="/analysis" />
-      </Tabs>
-    </Paper>);
+const styles = {
+  selected: {
+    color: '#2196f3!important'
   }
 }
+
+class Footer extends React.Component {
+  state = {
+    value: 'today',
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props
+    const { value } = this.state
+
+    return (
+      <BottomNavigation value={value} onChange={this.handleChange} className="footer">
+        <BottomNavigationAction classes={{selected: classes.selected}} value="today" icon={<Today />} label="TODAY" component={Link} to="/" replace />
+        <BottomNavigationAction classes={{selected: classes.selected}} value="history" icon={<History />} label="HISTORY" component={Link} to="/history" replace />
+        <BottomNavigationAction classes={{selected: classes.selected}} value="analysis" icon={<ShowChart />} label="ANALYSIS" component={Link} to="/analysis" replace />
+      </BottomNavigation>
+    );
+  }
+}
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Footer)
--- src/pages/today/today.jsx
+++ src/pages/today/today.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
+import Select from '@/components/select/select'
 import './today.scss'
 
 class Home extends React.Component {
   render () {
     return (
       <div className="App">
-        123
+        <Select></Select>
       </div>
     );
   }
